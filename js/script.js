6fa34bf047f91a4d886abb63cae67b1009a2d69a
italocirillo <italocirillo@gmail.com>
2023-03-28 18:59:21 +0200

    ESERCIZIO COMPLETO + BONUS

--- js/script.js
+++ js/script.js
@@ -8,6 +8,18 @@ createApp({
             message: "Hello Vue",
             imgRandom: "https://picsum.photos/200/300",
             imgAlt: "immagine random",
+            coloreTitolo: "",
         }
     },
+    methods: {
+        cambiaColoreRosso(){
+            this.coloreTitolo="rosso";
+        },
+        cambiaColoreBianco(){
+            this.coloreTitolo="bianco";
+        },
+        cambiaColoreVerde(){
+            this.coloreTitolo="verde";
+        }
+    }
 }).mount("#app");
