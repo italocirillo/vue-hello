c6d13b104295bc39a9699a6062143d472bb74494
italocirillo <italocirillo@gmail.com>
2023-03-28 17:41:10 +0200

    BONUS IMMAGINE FATTO

--- js/script.js
+++ js/script.js
@@ -6,6 +6,8 @@ createApp({
     data() {
         return {
             message: "Hello Vue",
+            imgRandom: "https://picsum.photos/200/300",
+            imgAlt: "immagine random",
         }
     },
 }).mount("#app");
